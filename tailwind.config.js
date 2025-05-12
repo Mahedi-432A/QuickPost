85387d5c1a9c2a96b7a7cbbe87198b8aeb30845f
Mahedi-432A <mdmahedih038@gmail.com>
2025-05-13 03:12:33 +0600

    Add Footer component and integrate daisyUI for styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+/* global require*/
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
@@ -7,5 +9,7 @@ export default {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('daisyui'),
+  ],
 }
